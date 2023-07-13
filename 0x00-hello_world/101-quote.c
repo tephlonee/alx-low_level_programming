c515d83200c0004fb6662491fa724a13ea0296cb
Tijani Oboirien <oboirientijani@gmail.com>
2023-07-13 15:43:57 -0700

    First set of C projects

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main -returns a print statement
+ * returns - 1
+ */
+int main(void){
+	write(2 , "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - prints a string
+ * returns - 0 as always
+ */
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,")
+	return (0);
+}
